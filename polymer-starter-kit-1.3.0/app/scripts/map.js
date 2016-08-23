73f5845d44b8daf6611299c5ce4a6524fca117a0
olenakozdoba <aljona.kozdoba@gmail.com>
2016-08-23 06:47:03 -0700

    added popup window on marker click

--- polymer-starter-kit-1.3.0/app/scripts/map.js
+++ polymer-starter-kit-1.3.0/app/scripts/map.js
@@ -3,8 +3,10 @@
   // failed.", it means you probably did not give permission for the browser to
   // locate you.function initMap() {
 function initMap() {
+  google.maps.InfoWindow.prototype.isOpened = false;
+
   var map = new google.maps.Map(document.getElementById('chargemap'), {
-    center: {lat: -34.397, lng: 150.644},
+    center: {lat: 34.146, lng: -118.130},//34.1463769,-118.1298394
     zoom: 15
   });
   var infoWindow = new google.maps.InfoWindow({map: map});
@@ -27,22 +29,15 @@ function initMap() {
 	  
 	  spotRef.on("value", function(snapshot) {
 	  
-		  var addresses = [];
+		  var spots = [];
 		  snapshot.forEach(function(data) {
-			addresses.push(data.val()["address"]);
+			spots.push({"address" : data.val()["address"], "price" : data.val()["price"]});
 		  });
 		
-		  for (var x = 0; x < addresses.length; x++) {
-			  $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
-				  var p = data.results[0].geometry.location
-				  var latlng = new google.maps.LatLng(p.lat, p.lng);
-				  new google.maps.Marker({
-					  position: latlng,
-					  map: map
-				  });
-
-			  });
+		  for (var x = 0; x < spots.length; x++) {
+        createMarker(spots[x], map);
 		  }
+
 		}, function (errorObject) {
 		  console.log("The read failed: " + errorObject.code);
 		});
@@ -54,6 +49,31 @@ function initMap() {
     // Browser doesn't support Geolocation
     handleLocationError(false, infoWindow, map.getCenter());
   }
+}
+
+function createMarker(spot, map) {
+  $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?address='+spot.address+'&sensor=false', null, function (data) {
+    var p = data.results[0].geometry.location
+    var latlng = new google.maps.LatLng(p.lat, p.lng);
+    var marker = new google.maps.Marker({
+      position: latlng,
+      map: map
+    });
+    var infowindow = new google.maps.InfoWindow({
+      content: "price: $" + spot.price + " per hour"
+    });
+
+    marker.addListener('click', function() {
+      if (!infowindow.isOpened) {
+        infowindow.open(marker.get('chargemap'), marker);
+        infowindow.isOpened = true;
+      }
+      else {
+        infowindow.close();
+        infowindow.isOpened = false;
+      }
+    });
+  });
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
