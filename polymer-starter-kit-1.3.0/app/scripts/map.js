97697c605887051ae3d3df418e62036458566a3d
liutztomato <liutzcn@msn.com>
2016-08-24 13:31:43 -0700

    Add a reserve/release button to markers

--- polymer-starter-kit-1.3.0/app/scripts/map.js
+++ polymer-starter-kit-1.3.0/app/scripts/map.js
@@ -9,7 +9,7 @@ function initMap() {
     center: {lat: 34.146, lng: -118.130},//34.1463769,-118.1298394
     zoom: 15
   });
-  var infoWindow = new google.maps.InfoWindow({map: map});
+  //var infoWindow = new google.maps.InfoWindow({map: map});
   
   // Try HTML5 geolocation.
   if (navigator.geolocation) {
@@ -19,8 +19,16 @@ function initMap() {
         lng: position.coords.longitude
       };
 
-      infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found.');
+      //infoWindow.setPosition(pos);
+      //infoWindow.setContent('Location found.');
+	  
+	  var image = 'http://i.stack.imgur.com/orZ4x.png';
+	  var marker = new google.maps.Marker({
+		  position: pos,
+		  map: map,
+		  icon: image
+	  });  
+	  marker.setMap(map);
       map.setCenter(pos);
 
       var ref = new Firebase("https://easychargeapp.firebaseio.com");
@@ -31,17 +39,17 @@ function initMap() {
 	  
 		  var spots = [];
 		  snapshot.forEach(function(data) {
-			spots.push({"address" : data.val()["address"], "price" : data.val()["price"]});
+			spots.push({"key" : data.key(), "address" : data.val()["address"], "price" : data.val()["price"]});
 		  });
 		
 		  for (var x = 0; x < spots.length; x++) {
-        createMarker(spots[x], map);
+			createMarker(spots[x], map);
 		  }
 
 		}, function (errorObject) {
 		  console.log("The read failed: " + errorObject.code);
 		});
-	  
+		
     }, function() {
       handleLocationError(true, infoWindow, map.getCenter());
     });
@@ -55,16 +63,45 @@ function createMarker(spot, map) {
   $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?address='+spot.address+'&sensor=false', null, function (data) {
     var p = data.results[0].geometry.location
     var latlng = new google.maps.LatLng(p.lat, p.lng);
-    var marker = new google.maps.Marker({
-      position: latlng,
-      map: map
-    });
-    var infowindow = new google.maps.InfoWindow({
-      content: "price: $" + spot.price + " per hour"
-    });
+	
+	var spotRef = new Firebase("https://easychargeapp.firebaseio.com/Spot");
+	  
+	var addressRef = spotRef.child(spot.key);
+	var status = 0;
+	
+	addressRef.on("value", function(snapshot) {
+	  status = snapshot.val()["status"];
+	}, function (errorObject) {
+	  console.log("The read failed: " + errorObject.code);
+	});
+	
+	if(status == 0) {
+		var marker = new google.maps.Marker({
+		  position: latlng,
+		  map: map
+		});
+		
+		var infowindow = new google.maps.InfoWindow({
+		  content: '<p>price: $' + spot.price + ' per hour</p> <button onclick="reserveButtonClicked(\''+spot.key+'\', true)">Reserve</button>'
+		});
+	}
+	else {
+		var image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+		var marker = new google.maps.Marker({
+		  position: latlng,
+		  map: map,
+		  icon: image
+		});
+		
+		var infowindow = new google.maps.InfoWindow({
+		  content: '<p>price: $' + spot.price + ' per hour</p> <button onclick="reserveButtonClicked(\''+spot.key+'\', false)">Release</button>'
+		});
+	}
+	
 
     marker.addListener('click', function() {
       if (!infowindow.isOpened) {
+	  
         infowindow.open(marker.get('chargemap'), marker);
         infowindow.isOpened = true;
       }
@@ -74,6 +111,17 @@ function createMarker(spot, map) {
       }
     });
   });
+}
+
+function reserveButtonClicked(spotKey, isReserving) {
+	var spotRef = new Firebase("https://easychargeapp.firebaseio.com/Spot");
+	  
+	var addressRef = spotRef.child(spotKey);
+	addressRef.update({
+	  "status": isReserving ? 1 : 0
+	});
+	
+	location.reload();
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
